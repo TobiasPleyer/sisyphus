1f311dfb10225b1455b9dddf469afc36112cd8c4
TobiasPleyer <tobi.pleyer@gmail.com>
2018-05-25 17:45:58 +0200

    Extend C output with action functions

--- src/Sisyphus/Language/C/Output.hs
+++ src/Sisyphus/Language/C/Output.hs
@@ -1,4 +1,25 @@
 module Sisyphus.Language.C.Output where
 
 
-outputSimple = undefined
+import Control.Exception (bracket)
+import System.IO (IOMode(..), openFile, hClose, hPutStr, hPutStrLn)
+import Sisyphus.Types
+import Sisyphus.Util
+import Sisyphus.Language.C.Util
+
+
+outputSimple :: RawStateMachine -> IO ()
+outputSimple rsm = do
+  let fsm_name = maybe "FSM" id (rsmName rsm)
+      header_file = fsm_name ++ ".h"
+      source_file = fsm_name ++ ".c"
+      header_content = interleave_shows nl2 [fsm_events
+                                            ,fsm_states
+                                            ,fsm_action_protos]
+      fsm_events = outputEventEnum fsm_name (rsmEvents rsm)
+      fsm_states = outputStateEnum fsm_name (rsmStates rsm)
+      fsm_action_protos = outputActionProtos (rsmActions rsm)
+  bracket
+    (openFile header_file WriteMode)
+    (hClose)
+    $ \fh -> hPutStr fh (header_content "")
--- src/Sisyphus/Language/C/Util.hs
+++ src/Sisyphus/Language/C/Util.hs
@@ -1,8 +1,8 @@
 module Sisyphus.Language.C.Util where
 
 
-import Sisyphus.Types (Event, State(..))
-import Sisyphus.Util (char, str, interleave_shows)
+import Sisyphus.Types
+import Sisyphus.Util
 
 
 outputSimpleEnum :: Maybe String  -- ^ optional name put at the end of the enum
@@ -22,12 +22,27 @@ outputSimpleEnum counter_name enum_name ss =
   . char ';'
 
 
-outputStateEnum :: [State] -> ShowS
-outputStateEnum = outputSimpleEnum (Just "MAX_NUM_STATES") "state_t" . map stName
+outputStateEnum :: String -> [State] -> ShowS
+outputStateEnum fsm_name = outputSimpleEnum (Just (fsm_name ++ "_NUM_STATES")) (fsm_name ++ "_state_t") . map stName
 
 
-outputEventEnum :: [Event] -> ShowS
-outputEventEnum = outputSimpleEnum (Just "MAX_NUM_EVENTS") "event_t"
+outputEventEnum :: String -> [Event] -> ShowS
+outputEventEnum fsm_name = outputSimpleEnum (Just (fsm_name ++ "_NUM_EVENTS")) (fsm_name ++ "_event_t")
+
+
+outputActionProtos :: [Action] -> ShowS
+outputActionProtos as =
+    str "typedef void action_function_t (void);" . nl2
+  . interleave_shows nl as'
+  where
+    as' = map (enclose "action_function_t " ";") as
+
+
+outputHandlerProtos :: RawStateMachine -> ShowS
+outputHandlerProtos rsm =
+  let states = rsmStates rsm
+      transitions = rsmTransitions rsm
+  in  str "typedef void handler_function_t (void);" . nl2
 
 
 outputTransitionTable = undefined
