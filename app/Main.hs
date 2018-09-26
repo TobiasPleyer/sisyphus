ad480d82b82b4ab50c3c9bec40ba5f148c2d3aba
TobiasPleyer <tobi.pleyer@gmail.com>
2018-09-26 19:07:10 +0200

    Move tryRenderTarget to Targets.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,9 @@
 module Main where
 
-import qualified Data.Map.Strict as M
-import System.Exit (ExitCode(..), exitWith)
 import Sisyphus.Lexer (tokenize)
 import Sisyphus.Parser (parse)
 import Sisyphus.Types
-import Sisyphus.Targets (supportedTargets)
-
-
-tryRenderTarget target vsm outFile = do
-  let maybeRenderFunc = M.lookup target supportedTargets
-  case maybeRenderFunc of
-    Nothing -> do
-      putStrLn $ "Target '" ++ target ++ "' is not supported!"
-      exitWith (ExitFailure 1)
-    Just renderFunc -> do
-      putStrLn $ "Rendering target '" ++ target ++ "' to file " ++ outFile ++ "..."
-      renderFunc vsm outFile
-      putStrLn "Done"
+import Sisyphus.Targets (supportedTargets, tryRenderTarget)
 
 
 main :: IO ()
--- src/Sisyphus/Targets.hs
+++ src/Sisyphus/Targets.hs
@@ -1,8 +1,9 @@
 module Sisyphus.Targets
-( supportedTargets
-)where
+
+where
 
 
+import System.Exit (ExitCode(..), exitWith)
 import qualified Data.Map.Strict as M
 import Sisyphus.Language.Graphviz (renderGvSimple)
 import Sisyphus.Language.C (renderCSimple)
@@ -10,3 +11,15 @@ import Sisyphus.Language.C (renderCSimple)
 
 supportedTargets = M.fromList [ ("Graphviz_Simple", renderGvSimple)
                               , ("C_Simple", renderCSimple)]
+
+
+tryRenderTarget target sm outFile = do
+  let maybeRenderFunc = M.lookup target supportedTargets
+  case maybeRenderFunc of
+    Nothing -> do
+      putStrLn $ "Target '" ++ target ++ "' is not supported!"
+      exitWith (ExitFailure 1)
+    Just renderFunc -> do
+      putStrLn $ "Rendering target '" ++ target ++ "' to file " ++ outFile ++ "..."
+      renderFunc sm outFile
+      putStrLn "Done"
