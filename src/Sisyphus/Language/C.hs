6ed9df63458c394f6d23fc77ceff3dc8d71c3b7c
TobiasPleyer <tobi.pleyer@gmail.com>
2018-10-11 20:27:14 +0200

    POC: Use conduit and ginger to render a target

--- src/Sisyphus/Language/C.hs
+++ src/Sisyphus/Language/C.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Sisyphus.Language.C
-( renderCSimple, mkCContext
+( renderCSimple
 ) where
 
 
+import Conduit
+import Data.Conduit
+import Data.Either (fromRight)
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import System.Exit (ExitCode(..), exitWith)
 import System.FilePath
 import Text.Ginger
@@ -18,23 +22,38 @@ import Sisyphus.Language.Template (defaultTemplateLoader, renderTemplate)
 
 cTemplateHeaderSimple = "C/fsm.h.tmpl"
 cTemplateSourceSimple = "C/fsm.c.tmpl"
+cTemplateSourceSimplePre = "C/fsm.c_pre.tmpl"
+cTemplateSourceSimplePost = "C/fsm.c_post.tmpl"
 
 
-renderCSimple :: StateMachine -> FilePath -> IO ()
-renderCSimple sm outDir = do
+renderCSimple = renderCSimple_Hybrid
+
+
+renderCSimple_GingerOnly :: StateMachine -> FilePath -> IO ()
+renderCSimple_GingerOnly sm outDir = do
+  renderCHeaderSimple_GingerOnly sm outDir
+  renderCSourceSimple_GingerOnly sm outDir
+
+
+renderCHeaderSimple_GingerOnly :: StateMachine -> FilePath -> IO ()
+renderCHeaderSimple_GingerOnly sm outDir = do
   let outHeaderFile = outDir </> (smName sm) <.> "h"
-  let outSourceFile = outDir </> (smName sm) <.> "c"
   cHeaderTempl <- parseGingerFile defaultTemplateLoader cTemplateHeaderSimple
   case cHeaderTempl of
     Left err -> do
-      putStrLn "Failed to load the header target!"
+      putStrLn "Failed to load the header template!"
     Right cHeaderTemplate -> do
       let cContext  = mkCContext sm
       renderTemplate cContext cHeaderTemplate outHeaderFile
+
+
+renderCSourceSimple_GingerOnly :: StateMachine -> FilePath -> IO ()
+renderCSourceSimple_GingerOnly sm outDir = do
+  let outSourceFile = outDir </> (smName sm) <.> "c"
   cSourceTempl <- parseGingerFile defaultTemplateLoader cTemplateSourceSimple
   case cSourceTempl of
     Left err -> do
-      putStrLn "Failed to load the source target!"
+      putStrLn "Failed to load the source template!"
     Right cSourceTemplate -> do
       let cContext  = mkCContext sm
       renderTemplate cContext cSourceTemplate outSourceFile
@@ -52,3 +71,30 @@ mkCContext sm = makeContextText contextLookup
     fsm_events = map T.pack $ smEvents sm
     fsm_states = map T.pack $ M.keys (smStates sm)
     fsm_actions = map T.pack $ smActions sm
+
+
+renderCSimple_Hybrid :: StateMachine -> FilePath -> IO ()
+renderCSimple_Hybrid sm outDir = do
+  renderCHeaderSimple_GingerOnly sm outDir
+  let
+    outSourceFile = outDir </> (smName sm) <.> "c"
+    cContext  = mkCContext sm
+  cSourceTemplatePre <- parseGingerFile defaultTemplateLoader cTemplateSourceSimplePre
+  case cSourceTemplatePre of
+    Left err -> do
+      putStrLn "Failed to load the source pre template!"
+    Right cSourceTemplatePre -> do
+      let pre = runGinger cContext cSourceTemplatePre
+      cSourceTemplatePost <- parseGingerFile defaultTemplateLoader cTemplateSourceSimplePost
+      case cSourceTemplatePost of
+        Left err -> do
+          putStrLn "Failed to load the source post template!"
+        Right cSourceTemplatePost -> do
+          let
+            post = runGinger cContext cSourceTemplatePost
+            source = do
+              yield pre
+              yield post
+          runConduitRes $ source
+                        .| encodeUtf8C
+                        .| sinkFile outSourceFile
