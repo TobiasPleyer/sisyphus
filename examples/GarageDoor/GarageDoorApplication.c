e5a17e31862955b36fdd82d701593e101ed26fa5
TobiasPleyer <tobi.pleyer@gmail.com>
2018-10-11 18:18:54 +0200

    Small improvements of the GarageDoor example

--- examples/GarageDoor/GarageDoorApplication.c
+++ examples/GarageDoor/GarageDoorApplication.c
@@ -9,12 +9,23 @@ int main()
     sm = GarageDoorFSM_New();
     GarageDoorFSM_Start(sm);
     int i = 0;
+    char* space = "  ";
+    char* arrow = "->";
+    GarageDoorFSM_event_t ev;
+    GarageDoorFSM_state_t st;
     while (1)
     {
-        printf("Current state: %s\n", GarageDoorFSM_state_to_str(sm->current_state));
+        char* pre;
+        printf("\n\n");
+        for(i=0; i<GarageDoorFSM_NUM_STATES; i++)
+        {
+            st = (GarageDoorFSM_state_t) i;
+            pre = (st == sm->current_state) ? arrow : space;
+            printf("%s%s\n", pre, GarageDoorFSM_state_to_str(st));
+        }
         scanf("%d", &i);
         if (i > GarageDoorFSM_NUM_EVENTS) break;
-        GarageDoorFSM_event_t ev = (GarageDoorFSM_event_t) i;
+        ev = (GarageDoorFSM_event_t) i;
         printf("Adding event '%s' to event queue\n", GarageDoorFSM_event_to_str(ev));
         GarageDoorFSM_AddEvent(sm, ev);
         GarageDoorFSM_Deduct(sm);
